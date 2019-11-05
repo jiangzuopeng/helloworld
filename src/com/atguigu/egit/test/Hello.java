a4852ae477adecdcd2250f338ff8db0d3bbc9759
jzp <851717720@qq.com>
2019-11-05 15:32:08 +0800

    更新啦

--- src/com/atguigu/egit/test/Hello.java
+++ src/com/atguigu/egit/test/Hello.java
@@ -3,6 +3,9 @@ package com.atguigu.egit.test;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		System.out.println("hello1");
+		System.out.println("hello2");
+		
 	}
 
 }
